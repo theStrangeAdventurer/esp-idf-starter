314a9c087e36bc779d41354d03449a0c5f5ca745
Alexey Zaytsev <alexej3ajtsev@icloud.com>
2023-09-19 21:51:58 +0600

    chore: wifi refoctor

--- main/main.c
+++ main/main.c
@@ -10,7 +10,6 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_mac.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
@@ -20,7 +19,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/param.h>
-#include "esp_netif.h"
 #include <esp_http_server.h>
 #include "esp_tls.h"
 #include <esp_system.h>
@@ -36,17 +34,6 @@
 #include <dirent.h>
 /* << FS*/
 
-
-/* The examples use WiFi configuration that you can set via project configuration menu.
-
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define WIFI_SSID "mywifissid"
-*/
-#define ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
-#define ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
-#define ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
-#define MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
-
 #define HTTP_QUERY_KEY_MAX_LEN  (64)
 
 #define LED GPIO_NUM_2
@@ -54,9 +41,13 @@
 static const char *TAG = "app"; // Tag for logger
 
 /* Definitions for server.c file >> */
-esp_err_t start_webserver(const char *base_path);
+httpd_handle_t start_webserver(const char *base_path);
 esp_err_t stop_webserver(httpd_handle_t server);
 /* << Definitions for server.c file */
+
+/* Definitions for wifi.c file >> */
+void wifi_init_softap(void);
+/* << Definitions for wifi.c file */
 
 /* configure FS */
 esp_err_t init_fs(void)
@@ -91,8 +82,6 @@ esp_err_t init_fs(void)
 }
 /* configure FS */
 
-#define CHECK_FILE_EXTENSION(filename, ext) (strcasecmp(&filename[strlen(filename) - strlen(ext)], ext) == 0)
-
 /* configure LED pin */
 void configure_led() {
     gpio_reset_pin(LED);
@@ -110,65 +99,6 @@ void led_off() {
 }
 
 /* configure LED pin */
-
-static void wifi_event_handler(void* arg, esp_event_base_t event_base,
-                                    int32_t event_id, void* event_data)
-{
-    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
-                 MAC2STR(event->mac), event->aid);
-    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
-                 MAC2STR(event->mac), event->aid);
-    }
-}
-
-void wifi_init_softap(void)
-{
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &wifi_event_handler,
-                                                        NULL,
-                                                        NULL));
-
-    wifi_config_t wifi_config = {
-        .ap = {
-            .ssid = ESP_WIFI_SSID,
-            .ssid_len = strlen(ESP_WIFI_SSID),
-            .channel = ESP_WIFI_CHANNEL,
-            .password = ESP_WIFI_PASS,
-            .max_connection = MAX_STA_CONN,
-#ifdef CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT
-            .authmode = WIFI_AUTH_WPA3_PSK,
-            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
-#else /* CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT */
-            .authmode = WIFI_AUTH_WPA2_PSK,
-#endif
-            .pmf_cfg = {
-                    .required = true,
-            },
-        },
-    };
-    if (strlen(ESP_WIFI_PASS) == 0) {
-        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-    }
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
-             ESP_WIFI_SSID, ESP_WIFI_PASS, ESP_WIFI_CHANNEL);
-}
 
 /**
  * HTTP Server handlers
